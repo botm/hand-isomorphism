10ca04ec3d432828302c6ef950ac56c490df4f17
Michael von Bothmer <michael@bothmer.name>
2016-03-02 22:19:01 +0100

    new file:   .classpath 	modified:   .gitignore 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	modified:   README.md 	new file:   src/mbot/poker/handindex/HandIndexer.java 	new file:   src/mbot/poker/handindex/HandIndexerState.java 	new file:   src/mbot/poker/handindex/Test.java

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hand-isomorphism
 A Java library for indexing poker hands that respects suit isomorphisms
+
+This is a direct port of Kevin Waugh's hand-isomorphism C library. You can find the original code here:
+https://github.com/kdub0/hand-isomorphism#poker-hand-isomorphisms
+
+Please see:
+K. Waugh, 2013. A Fast and Optimal Hand Isomorphism Algorithm. In the Second Computer Poker and Imperfect Information Symposium at AAAI
+http://www.cs.cmu.edu/~waugh/publications/isomorphism13.pdf
---
+++ src/mbot/poker/handindex/Test.java
@@ -0,0 +1,58 @@
+package mbot.poker.handindex;
+
+import java.util.Random;
+
+public class Test {
+
+	public static void testRandom(HandIndexer indexer) {
+		Random rnd = new Random();
+		
+		int totalCards = 0;
+		for(int i=0; i<indexer.rounds; ++i)
+			totalCards += indexer.cardsPerRound[i];
+		
+		int[] cards = new int[totalCards];
+		int[] cards2 = new int[totalCards];
+
+		for(long n=0; n<10000000L; ++n) {
+			long deadCardMask = 0;
+			for(int i=0; i<totalCards; ++i) {
+				do {
+					cards[i] = (int) (rnd.nextDouble() * HandIndexer.CARDS);
+				} while (((1L << cards[i]) & deadCardMask) != 0);
+				deadCardMask |= (1L << cards[i]);
+			}
+			long index  = indexer.indexLast(cards);
+
+			indexer.unindex(indexer.rounds-1, index, cards2);
+			long index2 = indexer.indexLast(cards2);
+			assert(index2 == index);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("testing hand-isomorphism...");  
+
+		HandIndexer preflopIndexer = new HandIndexer(2);
+		HandIndexer flopIndexer = new HandIndexer(2,3);
+		HandIndexer turnIndexer = new HandIndexer(2,3,1);
+		HandIndexer riverIndexer = new HandIndexer(2,3,1,1);
+
+		assert(preflopIndexer.roundSize[0] == 169);
+		assert(flopIndexer.roundSize[0] == 169);
+		assert(turnIndexer.roundSize[0] == 169);
+		assert(riverIndexer.roundSize[0] == 169);
+		assert(flopIndexer.roundSize[1] == 1286792);
+		assert(turnIndexer.roundSize[1] == 1286792);
+		assert(riverIndexer.roundSize[1] == 1286792);
+		assert(turnIndexer.roundSize[2] == 55190538);
+		assert(riverIndexer.roundSize[2] == 55190538);
+		assert(riverIndexer.roundSize[3] == 2428287420L);
+		
+		testRandom(flopIndexer);
+		testRandom(turnIndexer);
+		testRandom(riverIndexer);
+		
+		System.out.println("done!");  
+	}
+}
