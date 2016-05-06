88032fc8205f492fae38c5cc2f0de5e3d01b9333
Michael von Bothmer <michael@bothmer.name>
2016-05-06 20:46:16 +0200

    Added JavaDoc

--- src/mbot/poker/handindex/HandIndexerState.java
+++ src/mbot/poker/handindex/HandIndexerState.java
@@ -1,15 +1,15 @@
 package mbot.poker.handindex;
 
 public final class HandIndexerState {
-	
-	public final int[] suitIndex = new int[HandIndexer.SUITS];
-	public final int[] suitMultiplier = new int[HandIndexer.SUITS];
-	public int round, permutationIndex, permutationMultiplier;
-	public final int[] usedRanks = new int[HandIndexer.SUITS];
-	
-	public HandIndexerState() {
-		permutationMultiplier = 1;
-		for (int i=0; i<HandIndexer.SUITS; ++i)
-			suitMultiplier[i] = 1;
-	}
+
+  public final int[] suitIndex = new int[HandIndexer.SUITS];
+  public final int[] suitMultiplier = new int[HandIndexer.SUITS];
+  public int round, permutationIndex, permutationMultiplier;
+  public final int[] usedRanks = new int[HandIndexer.SUITS];
+
+  public HandIndexerState() {
+    permutationMultiplier = 1;
+    for (int i = 0; i < HandIndexer.SUITS; ++i)
+      suitMultiplier[i] = 1;
+  }
 }
--- src/mbot/poker/handindex/Test.java
+++ src/mbot/poker/handindex/Test.java
@@ -3,56 +3,55 @@ package mbot.poker.handindex;
 import java.util.Random;
 
 public class Test {
+  private static void testRandom(HandIndexer indexer) {
+    Random rnd = new Random();
 
-	public static void testRandom(HandIndexer indexer) {
-		Random rnd = new Random();
-		
-		int totalCards = 0;
-		for(int i=0; i<indexer.rounds; ++i)
-			totalCards += indexer.cardsPerRound[i];
-		
-		int[] cards = new int[totalCards];
-		int[] cards2 = new int[totalCards];
+    int totalCards = 0;
+    for (int i = 0; i < indexer.rounds; ++i)
+      totalCards += indexer.cardsPerRound[i];
 
-		for(long n=0; n<10000000L; ++n) {
-			long deadCardMask = 0;
-			for(int i=0; i<totalCards; ++i) {
-				do {
-					cards[i] = (int) (rnd.nextDouble() * HandIndexer.CARDS);
-				} while (((1L << cards[i]) & deadCardMask) != 0);
-				deadCardMask |= (1L << cards[i]);
-			}
-			long index  = indexer.indexLast(cards);
+    int[] cards = new int[totalCards];
+    int[] cards2 = new int[totalCards];
 
-			indexer.unindex(indexer.rounds-1, index, cards2);
-			long index2 = indexer.indexLast(cards2);
-			assert(index2 == index);
-		}
-	}
+    for (long n = 0; n < 10000000L; ++n) {
+      long deadCardMask = 0;
+      for (int i = 0; i < totalCards; ++i) {
+        do {
+          cards[i] = (int) (rnd.nextDouble() * HandIndexer.CARDS);
+        } while (((1L << cards[i]) & deadCardMask) != 0);
+        deadCardMask |= (1L << cards[i]);
+      }
+      long index = indexer.indexLast(cards);
 
-	public static void main(String[] args) {
-		System.out.println("testing hand-isomorphism...");  
+      indexer.unindex(indexer.rounds - 1, index, cards2);
+      long index2 = indexer.indexLast(cards2);
+      assert index2 == index;
+    }
+  }
 
-		HandIndexer preflopIndexer = new HandIndexer(2);
-		HandIndexer flopIndexer = new HandIndexer(2,3);
-		HandIndexer turnIndexer = new HandIndexer(2,3,1);
-		HandIndexer riverIndexer = new HandIndexer(2,3,1,1);
+  public static void main(String[] args) {
+    System.out.println("testing hand-isomorphism...");
 
-		assert(preflopIndexer.roundSize[0] == 169);
-		assert(flopIndexer.roundSize[0] == 169);
-		assert(turnIndexer.roundSize[0] == 169);
-		assert(riverIndexer.roundSize[0] == 169);
-		assert(flopIndexer.roundSize[1] == 1286792);
-		assert(turnIndexer.roundSize[1] == 1286792);
-		assert(riverIndexer.roundSize[1] == 1286792);
-		assert(turnIndexer.roundSize[2] == 55190538);
-		assert(riverIndexer.roundSize[2] == 55190538);
-		assert(riverIndexer.roundSize[3] == 2428287420L);
-		
-		testRandom(flopIndexer);
-		testRandom(turnIndexer);
-		testRandom(riverIndexer);
-		
-		System.out.println("done!");  
-	}
+    HandIndexer preflopIndexer = new HandIndexer(2);
+    HandIndexer flopIndexer = new HandIndexer(2, 3);
+    HandIndexer turnIndexer = new HandIndexer(2, 3, 1);
+    HandIndexer riverIndexer = new HandIndexer(2, 3, 1, 1);
+
+    assert preflopIndexer.roundSize[0] == 169;
+    assert flopIndexer.roundSize[0] == 169;
+    assert turnIndexer.roundSize[0] == 169;
+    assert riverIndexer.roundSize[0] == 169;
+    assert flopIndexer.roundSize[1] == 1286792;
+    assert turnIndexer.roundSize[1] == 1286792;
+    assert riverIndexer.roundSize[1] == 1286792;
+    assert turnIndexer.roundSize[2] == 55190538;
+    assert riverIndexer.roundSize[2] == 55190538;
+    assert riverIndexer.roundSize[3] == 2428287420L;
+
+    testRandom(flopIndexer);
+    testRandom(turnIndexer);
+    testRandom(riverIndexer);
+
+    System.out.println("done!");
+  }
 }
